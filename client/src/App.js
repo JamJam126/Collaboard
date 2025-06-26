8c61a0587311b9c24ce89a783f31100426873aac
VRithea <vongrithea126@gmail.com>
2025-06-26 12:06:15 +0700

    Initialize Commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import "./index.css"
+function App() {
+  return (
+    <div className="bg-blue-300">
+      a
+    </div>
+  );
+}
+
+export default App;
---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+// import express from "express";
+const express = require('express')
+const cors = require('cors')
+
+// require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+    res.json({ message: "API is working!" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

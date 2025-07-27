3bd232ab343f29ea57afa3e1598da1d62d9a4cae
HunDavin <davin.hun@student.cadt.edu.kh>
2025-07-27 16:29:01 +0700

    fixing auth api implementation

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const API_BASE_URL = "http://localhost:4000/api"
 
 export const loginApi=async(user)=>{
-    const res = await axios.post(`${API_BASE_URL}/user/login`, { email:user.email,password:user.password});
+    const res = await axios.post(`${API_BASE_URL}/auth/login`, { email:user.email,password:user.password});
     return res.data;
 }
 
 export const registerApi=async(user)=>{
-    const res = await axios.post(`${API_BASE_URL}/user/register`, { email:user.email,name:user.name,password:user.password});
+    const res = await axios.post(`${API_BASE_URL}/auth/register`, { email:user.email,name:user.name,password:user.password});
     return res.data;
 }
